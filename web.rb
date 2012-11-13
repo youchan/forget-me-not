97e242568cb11ababec9d222641b11fdaa97c5b1
youchan <youchan01@gmail.com>
2012-11-13 19:55:21 +0900

    temporary

--- web.rb
+++ web.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
+require 'sequel'
 
+DB = Sequel.sqlite("forget-me-not.db");
+
+DB.create_table :items do
+  primary_key :id
+  
+end
 
 get '/calendar' do
   @iframe = "<iframe src=\"https://www.google.com/calendar/embed?mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=j1cjb6hag0khuducbjlpoq6sj8%40group.calendar.google.com&amp;color=%2388880E&amp;src=7ffhsgeogne8u4flu5s695o5j4%40group.calendar.google.com&amp;color=%23333333&amp;src=youchan01%40gmail.com&amp;color=%232952A3&amp;ctz=Asia%2FTokyo\" style=\" border-width:0 \" width=\"800\" height=\"600\" frameborder=\"0\" scrolling=\"no\"></iframe>"
   erb :index 
+end
+
+get '/todo' do
+
 end
