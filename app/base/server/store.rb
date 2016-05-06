b476bde32de78d5c18a1d2f8aab2d7ce40b89861
youchan <youchan01@gmail.com>
2016-05-06 20:37:07 +0900

    Save to file on server store

--- app/base/server/store.rb
+++ app/base/server/store.rb
@@ -1,7 +1,11 @@
 class Store
-  def initialize
+  DEFAULT_DB_DIR = './.store'
+
+  def initialize(db_dir = DEFAULT_DB_DIR)
     @tables = {}
     @guid = SecureRandom.uuid
+    @db_dir = db_dir
+    Dir.mkdir(@db_dir) unless Dir.exist?(@db_dir)
   end
 
   def self.instance
@@ -18,10 +22,19 @@ class Store
 
   def save(model)
     @tables[model.class][model.id] = model
+
+    File.open(@db_dir + "/#{model.class}.db", "w") do |file|
+      file.write @tables[model.class].values.to_json
+    end
+
     yield model if block_given?
   end
 
   def fetch(model_class)
+    File.open(@db_dir + "/#{model_class}.db") do |file|
+      @tables[model_class] = JSON.parse(file.read).map {|m| [m["id"], model_class.new(m)] }.to_h
+    end
+
     yield @tables[model_class].values || [] if block_given?
   end
 end
