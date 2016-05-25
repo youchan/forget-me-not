c7d0ece0a105340b822da4684347357a88f13511
youchan <youchan01@gmail.com>
2016-05-25 17:44:26 +0900

    Add Hyalite::Sortable

---
+++ app/base/hyalite/sortable.rb
@@ -0,0 +1,51 @@
+require 'delegate'
+
+module Hyalite
+  module Sortable
+    def self.create
+      _config = Config.new
+      yield _config
+
+      Class.new {
+        include Component
+        include Component::ShortHand
+        include Sortable
+
+        def self.config=(config)
+          @config = config
+        end
+
+        def self.config
+          @config
+        end
+      }.tap{|cl| cl.config = _config }
+    end
+
+    def initialize
+      @config = self.class.config
+    end
+
+    def render
+      @config.wrap.el({className: 'hyalite-sortable'},
+        @props[:collection].sort_by(&@config.sort).map {|model| @config.component.el({@config.prop_key => model}) }
+      )
+    end
+
+    class Config
+      attr_accessor :wrap, :component, :prop_key
+      attr_reader :sort
+
+      def sort_by(field_name = nil, &block)
+        if field_name
+          if block
+            @sort = -> (model) { block.call(model.fields[field_name]) }
+          else
+            @sort = -> (model) { model.fields[field_name] }
+          end
+        else
+          @sort = block
+        end
+      end
+    end
+  end
+end
