fc1f0bca02389b5aa38615dc33c02a05947f40fd
youchan <youchan01@gmail.com>
2016-05-29 13:54:32 +0900

    Filter todo done on rescheduling

--- app/scheduler.rb
+++ app/scheduler.rb
@@ -21,20 +21,22 @@ module ForgetMeNot
         at = @scope.begin
       end
 
-      Entry.fetch(order: :order) do |entries|
-        entries.each do |entry|
+      Entry.fetch(filter: { done: false }, order: :order) do |entries|
+        time_boxes = entries.each_with_object([]) do |entry, time_boxes|
           duration = @scope.end - at
           pomodoro = entry.pomodoro
           while duration < pomodoro
-            TimeBox.create(entry_id: entry.id, pomodoro: duration, date: date, start_at: at.to_s)
+            time_boxes << TimeBox.new(entry_id: entry.id, pomodoro: duration, date: date, start_at: at.to_s)
             at = @scope.begin
             date = date.next_day
             pomodoro -= duration
             duration = @scope.end - @scope.begin
           end
-          TimeBox.create(entry_id: entry.id, pomodoro: pomodoro, date: date, start_at: at.to_s)
+          time_boxes << TimeBox.new(entry_id: entry.id, pomodoro: pomodoro, date: date, start_at: at.to_s)
           at = at.next(pomodoro)
         end
+
+        TimeBox.save(time_boxes)
       end
     end
 
